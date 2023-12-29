bcaba1efe958087b87e7c681fab854482db19987
vikasbalu <vikasbalu012@gmail.com>
2023-12-29 15:27:40 -0500

    Reworked Code

--- Hangman.py
+++ Hangman.py
@@ -22,30 +22,41 @@ def hangman():
     attempts_left = 6
 
     print("Welcome to Hangman!")
+
     print(display_board(word, guessed_letters))
 
     while attempts_left > 0 :
+
         guess = input("\n Guess a letter: ").lower()
         print(" The Number of attempts left are: ",attempts_left)
 
         if guess.isalpha() and len(guess) == 1: # keep looping until the user wins or loses
+           
             if guess in guessed_letters:
                 print("Already Gussed")
+
             elif guess in word:
                     guessed_letters.append(guess)
                     print(f"\nGood job, {guess} is in the word.")
+            
             else:
                 attempts_left -= 1
                 print(f"\nWrong choice. You have {attempts_left} attempts left.")
+        
         else:
             print("Invalid Input")
 
+        
+
         print(display_board(word, guessed_letters))
 
-    if "-" not in display_board(word, guessed_letters):
+        if word==guessed_letters:
+            break
+
+    if "-" in display_board(word, guessed_letters):
         print("you guessed it")
     
-    if "-" in display_board(word, guessed_letters):
+    if "-" not in display_board(word, guessed_letters):
         print("you have not  guessed it")
      
 if __name__ == "__main__":
